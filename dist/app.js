8c7d99d5f2ffb75c4471a8f388f778910fe4fd35
gnobu <inyangubong17@gmail.com>
2022-10-28 15:30:27 +0100

    added response to root route

--- dist/app.js
+++ dist/app.js
@@ -3,10 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
 const express_1 = __importDefault(require("express"));
-const PORT = 5000;
-const URL = 'http://localhost:5000';
+const PORT = process.env.PORT || 5000;
+const URL = process.env.URL || 'http://localhost:5000';
 const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send(`Head to "${URL}/api/task1" to get the task1 response`);
+});
 app.get('/api/task1', (req, res) => {
     const task1 = {
         slackUsername: 'gnobu',
