590a85ba8619c35d1e11189ab76be636bd968db8
gnobu <inyangubong17@gmail.com>
2022-11-04 20:06:15 +0100

    added task2 control to zuri and corresponding types to interface.ts

--- src/controllers/zuri.controller.ts
+++ src/controllers/zuri.controller.ts
@@ -1,5 +1,5 @@
 import * as express from 'express'
-import { IController } from '../interfaces/controller.interface';
+import { IController, IOperationBody, OPERATION } from '../interfaces/controller.interface';
 
 class Zuri implements IController {
     public path = '/zuri'
@@ -11,6 +11,7 @@ class Zuri implements IController {
 
     public initializeRoutes() {
         this.router.get(`${this.path}/task1`, this.task1)
+        this.router.post(`${this.path}/task2`, this.task2)
     }
 
     private task1 = (req: express.Request, res: express.Response) => {
@@ -26,6 +27,27 @@ class Zuri implements IController {
             bio: "Nothing to see here. Just a MERN stack developer"
         }
         res.json(task1)
+    }
+
+    private task2 = (req: express.Request, res: express.Response) => {
+        const quest: IOperationBody = req.body
+        let result
+        switch (quest.operation_type.toLowerCase()) {
+            case OPERATION.addition:
+                result = +quest.x + +quest.y
+                return res.json({ "slackUsername": "gnobu", result, "operation_type": OPERATION.addition })
+            case OPERATION.subtraction:
+                result = +quest.x - +quest.y
+                return res.json({ "slackUsername": "gnobu", result, "operation_type": OPERATION.subtraction })
+            case OPERATION.multiplication:
+                result = +quest.x * +quest.y
+                return res.json({ "slackUsername": "gnobu", result, "operation_type": OPERATION.multiplication })
+            case OPERATION.division:
+                result = parseFloat((+quest.x / +quest.y).toFixed(2))
+                return res.json({ "slackUsername": "gnobu", result, "operation_type": OPERATION.division })
+            default:
+                res.json({ "slackUsername": "gnobu", "result": null, "operation_type": quest.operation_type })
+        }
     }
 }
 
--- src/interfaces/controller.interface.ts
+++ src/interfaces/controller.interface.ts
@@ -4,3 +4,16 @@ export interface IController {
     path: string;
     router: Router
 }
+
+export enum OPERATION {
+    addition = 'addition',
+    subtraction = 'subtraction',
+    multiplication = 'multiplication',
+    division = 'division'
+}
+
+export interface IOperationBody {
+    operation_type: OPERATION;
+    x: number;
+    y: number;
+}
